a359fd420204d6ebbe64d6105f0df7e8e5eb0526
Caio Bello Zamana <caiobzm@gmail.com>
2023-05-01 20:59:42 -0300

    att

--- tipos-e-variaveis/src/TiposEvariaveis4.java
+++ tipos-e-variaveis/src/TiposEvariaveis4.java
@@ -2,7 +2,7 @@ public class TiposEvariaveis4 {
 
     public static void main(String[] args) {
         //variável em caixa baixa por de ser alterada, por convenção
-        
+
         int numero =5;
         numero = 10;
 
@@ -13,6 +13,9 @@ public class TiposEvariaveis4 {
 
         final double VALOR_DE_PI = 3.14;
         System.out.println(VALOR_DE_PI);
+
+        //classes com variáveis
+        String meuNome = "Caio B.";
     }
     
 }
