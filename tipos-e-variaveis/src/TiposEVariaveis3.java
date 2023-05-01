5d5c99a4f5a68752fb139209b417f7f85ec6954e
Caio Bello Zamana <caiobzm@gmail.com>
2023-05-01 20:44:10 -0300

    att

--- tipos-e-variaveis/src/TiposEVariaveis3.java
+++ tipos-e-variaveis/src/TiposEVariaveis3.java
@@ -2,8 +2,13 @@ public class TiposEVariaveis3 {
 
     public static void main(String[] args) {
         double salarioMinimo = 2500.23;
+        byte idade = 123;
+        short ano = 2031;
+        int cep = 12312333;
+        long cpf = 4687987965L;
+        float pi = 3.14F;
+    
 
-        
         
         //casting
         short numeroCurto = 1;
