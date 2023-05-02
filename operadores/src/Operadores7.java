e96e76c7776b96cadea39e0c6306228eda373d9c
Caio Bello Zamana <caiobzm@gmail.com>
2023-05-01 23:07:02 -0300

    att

--- operadores/src/Operadores7.java
+++ operadores/src/Operadores7.java
@@ -7,7 +7,7 @@ public class Operadores7 {
         boolean condicao1 = true;
         boolean condicao2 = false;
 
-        if (condicao1 && condicao2) {
+        if (condicao1 && condicao2) { // Se as duas forem verdadeira
             System.out.println("As duas condicoes sao verdadeiras");
         } else if (condicao1 || condicao2) { // Se uma ou outra for verdadeira
             System.out.println("Pelo menos uma das expressoes é verdadeira");
